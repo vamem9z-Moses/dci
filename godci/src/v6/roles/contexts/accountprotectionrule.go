ab1e56c030666b619815552eb8fe0885c67e7563
Moses E. Miles III <vamem9z@gmail.com>
2015-06-09 16:38:13 -0400

    Refactor AccountProtection rule to support multiple contexts

---
+++ godci/src/v6/roles/contexts/accountprotectionrule.go
@@ -0,0 +1,39 @@
+package contexts
+
+import (
+	"errors"
+	"v6/data"
+	"v6/domains"
+	"v6/roles/rules"
+)
+
+type AccountProtection struct{}
+
+func (ap AccountProtection) Action(ctx rules.Contexter) error {
+	switch accCtx := ctx.(type) {
+	default:
+		return errors.New("Not the correct context")
+	case *AccountWithdrawContext:
+		if !accCtx.Account.HasSufficentFunds(accCtx.Amount) {
+			userID := accCtx.Account.UserID
+			us := data.UserService{}
+			user, err := us.FindUser(userID)
+			if err != nil {
+				return err
+			}
+			savingsAccount, err := user.Accounts.GetAccount(domains.SAVINGSACCOUNT)
+			if err != nil {
+				return errors.New("No savings account, Insufficient funds")
+			}
+			if (savingsAccount.Balance() + accCtx.Account.Balance()) < accCtx.Amount {
+				return errors.New("Insufficient funds in both accounts")
+			}
+			transferAmt := accCtx.Amount - accCtx.Account.Balance()
+			transferCtx := TransferMoneyContext{}
+			transferCtx.Initialize(transferAmt, savingsAccount, accCtx.Account.AccountDomain)
+			transferCtx.Execute()
+		}
+
+	}
+	return nil
+}
