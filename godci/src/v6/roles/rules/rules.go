00951e7655d5867b597e310a6d57425092458512
Moses E. Miles III <vamem9z@gmail.com>
2015-06-14 12:00:30 -0400

    Remove unnessary check in ApplyRules function

--- godci/src/v6/roles/rules/rules.go
+++ godci/src/v6/roles/rules/rules.go
@@ -12,12 +12,10 @@ type BusinessRuler interface {
 type RulesRunner struct{}
 
 func (rr RulesRunner) ApplyRules(rules []BusinessRuler, ctx Contexter) error {
-	if len(rules) > 0 {
-		for _, rule := range rules {
-			err := rule.Action(ctx)
-			if err != nil {
-				return err
-			}
+	for _, rule := range rules {
+		err := rule.Action(ctx)
+		if err != nil {
+			return err
 		}
 	}
 	return nil
